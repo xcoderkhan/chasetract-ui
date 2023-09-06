ca742eb976ce450d3cb303ba6dc967a98ef26e89
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-09-06 18:13:36 +0600

    logo added

--- src/layout/shared/NavBar/NavBar.jsx
+++ src/layout/shared/NavBar/NavBar.jsx
@@ -1,11 +1,15 @@
+// import { Link } from 'react-router-dom';
 
+
+import { Link } from "react-router-dom";
+import logo from '../../../assets/images/logo/logo.png';
 
 const NavBar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -20,7 +24,10 @@ const NavBar = () => {
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link to='/' className="flex items-center gap-2 sm:justify-center normal-case text-xl">
+                    <img src={logo} alt="" className=" w-12 h-12" /> ChaseTrack
+                </Link>
+
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
